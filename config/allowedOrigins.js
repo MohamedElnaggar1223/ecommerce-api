e4da726c427cd93eec89a34962036b587112cb1b
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2023-11-15 14:19:28 +0200

    Final

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3000', 'http://192.168.1.3:3000', 'http://localhost:3002', 'https://checkout.stripe.com/c/pay/*']
+const allowedOrigins = ['https://njshop.vercel.app', 'http://localhost:3000', 'http://192.168.1.3:3000', 'http://localhost:3002', 'https://checkout.stripe.com/c/pay/*']
 
 module.exports = allowedOrigins
